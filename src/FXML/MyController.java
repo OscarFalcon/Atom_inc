d941a40ea14237eeff3e9e3f7c7fd30556c1a5cd
oscarfalcon <oscar.falcon@live.com>
2014-05-23 16:25:37 -0500

    submit

--- src/FXML/MyController.java
+++ src/FXML/MyController.java
@@ -1,6 +1,7 @@
 package FXML;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -12,15 +13,41 @@ import javafx.scene.layout.GridPane;
 import Custom.NumberTextField;
 
 public class MyController implements Initializable{
+	
 	@FXML private CheckBox OK1, OK2, OK3, OK4, OK5;
+	
+	
 	@FXML private CheckBox NOTOK1, NOTOK2, NOTOK3, NOTOK4, NOTOK5;
-	private CheckBox[][] checkboxes = new CheckBox[][]{ {OK1, NOTOK1}, {OK2, NOTOK2}, {OK3, NOTOK3}, {OK4, NOTOK4}, {OK5, NOTOK5}};
+	
+	
+	
+	private CheckBox[][] checkboxes = new CheckBox[][]{
+			{OK1, NOTOK1}, {OK2, NOTOK2}, {OK3, NOTOK3}, {OK4, NOTOK4}, {OK5, NOTOK5}};
+	
+	
 	@FXML private Label CUST, DATE, TAGS, YEAR, MAKE, MODEL, WO, LICNUM, VIN, ENG, TRANS, MILES;
+	
 	@FXML GridPane grid;
 	
 	public void checked(ActionEvent event){
 		int place, j=1;
+		CheckBox box;
 		
+		box = (CheckBox) event.getSource();
+		
+		if(box.isSelected()){
+			box.getStyleClass().remove("check-box-regular");
+			box.getStyleClass().add("check-box-valid");
+		}
+		else{
+			box.getStyleClass().remove("check-box-valid");
+			box.getStyleClass().add("check-box-regular");
+		}
+		
+		
+/**		
+ * 
+ * 
 		// Get the box that was checked!
 		//place is what line it is located with respect to corresponding values
 		for(place = 0; place < checkboxes.length; place++){
@@ -52,17 +79,19 @@ public class MyController implements Initializable{
 		//test.getStyleClass().remove("check-box-regular"); // standard grey color
 		//test.getStyleClass().add("check-box-valid"); // green color
 													// need to make red
-		
+		*/
 	}
 
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		//test.setSelected(true);
+		//OK1.setSelected(true);
+		//OK1.getStyleClass().remove("check-box-regular");
+		//OK1.getStyleClass().add("check-box-valid");
 		//cust.setText("TESTING");
 		
-		grid.add(new NumberTextField(),6, 15);
+		//grid.add(new NumberTextField(),6, 15);
 	}
 		
 	
