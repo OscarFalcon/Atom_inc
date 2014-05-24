a58f084678474f96cc592e9bae7ca69bf323e848
oscarfalcon <oscar.falcon@live.com>
2014-05-23 20:55:32 -0500

    excel improvements

--- src/FXML/MyController.java
+++ src/FXML/MyController.java
@@ -2,6 +2,7 @@ package FXML;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -24,29 +25,29 @@ import Custom.NumberTextField;
 public class MyController implements Initializable{
 	
 	/** title fields **/
-	@FXML private static Label CUST, DATE, TAGS, YEAR, MAKE, MODEL, WO, LICNUM, VIN, ENG, TRANS, MILES;
+	@FXML private Label CUST, DATE, TAGS, YEAR, MAKE, MODEL, WO, LICNUM, VIN, ENG, TRANS, MILES;
 	
 	/**OK CHECK BOXES **/
-	@FXML private static CheckBox OK1, OK2, OK3, OK4, OK5, OK6, OK7, OK8, OK9, OK10, OK11, OK12, OK13,OK14,OK15, OK16, OK17, OK18, OK19, OK20, OK21, OK22
+	@FXML private CheckBox OK1, OK2, OK3, OK4, OK5, OK6, OK7, OK8, OK9, OK10, OK11, OK12, OK13,OK14,OK15, OK16, OK17, OK18, OK19, OK20, OK21, OK22
 	, OK23, OK24, OK25, OK26, OK27, OK28, OK29, OK30, OK31, OK32, OK33, OK34, OK35, OK36, OK37, OK38, OK39, OK40, OK41, OK42, OK43, OK44, OK45, OK46
 	, OK47, OK48, OK49;
 	
 	/** NOT OK CHECK BOXES **/
-	@FXML private static CheckBox NOTOK1, NOTOK2, NOTOK3, NOTOK4, NOTOK5, NOTOK6, NOTOK7, NOTOK8, NOTOK9, NOTOK10, NOTOK11, NOTOK12, NOTOK13, NOTOK14, NOTOK15
+	@FXML private CheckBox NOTOK1, NOTOK2, NOTOK3, NOTOK4, NOTOK5, NOTOK6, NOTOK7, NOTOK8, NOTOK9, NOTOK10, NOTOK11, NOTOK12, NOTOK13, NOTOK14, NOTOK15
 		,NOTOK16, NOTOK17, NOTOK18, NOTOK19, NOTOK20, NOTOK21, NOTOK22, NOTOK23, NOTOK24, NOTOK25, NOTOK26, NOTOK27, NOTOK28, NOTOK29, NOTOK30
 		,NOTOK31, NOTOK32, NOTOK33,NOTOK34, NOTOK35, NOTOK36, NOTOK37, NOTOK38, NOTOK39, NOTOK40, NOTOK41, NOTOK42, NOTOK43, NOTOK44, NOTOK45 
 		,NOTOK46, NOTOK47, NOTOK48, NOTOK49;
 	
 	/** TECHNICIANS' COMMENTS **/
-	@FXML private static ComboBox TC1,TC2,TC3,TC4,TC5,TC6,TC7,TC8,TC9,TC10,TC11,TC12,TC13,TC14,TC15,TC16,TC17,TC18,TC19,TC20,TC21,TC22,TC23,TC24,TC25
+	@FXML private  ComboBox TC1,TC2,TC3,TC4,TC5,TC6,TC7,TC8,TC9,TC10,TC11,TC12,TC13,TC14,TC15,TC16,TC17,TC18,TC19,TC20,TC21,TC22,TC23,TC24,TC25
 					,TC26,TC27,TC28,TC29,TC30,TC31,TC32,TC33,TC34,TC35,TC36,TC37,TC38,TC39,TC40,TC41,TC42,TC43,TC44,TC45,TC46,TC147,TC48,TC49;
 	
 	/** RECOMMENDED REPAIRS  **/
-	@FXML private static ComboBox RR1,RR2,RR3,RR4,RR5,RR6,RR7,RR8,RR9,RR10,RR11,RR12,RR13,RR14,RR15,RR16,RR17,RR18,RR19,RR20,RR21,RR22,RR23,RR24,RR25,RR26
+	@FXML private ComboBox RR1,RR2,RR3,RR4,RR5,RR6,RR7,RR8,RR9,RR10,RR11,RR12,RR13,RR14,RR15,RR16,RR17,RR18,RR19,RR20,RR21,RR22,RR23,RR24,RR25,RR26
 					,RR27,RR28,RR29,RR30,RR31,RR32,RR33,RR34,RR35,RR36,RR37,RR38,RR39,RR40,RR41,RR42,RR43,RR44,RR45,RR46,RR47,RR48,RR49;
 	
 	/** PRIORITY **/
-	@FXML private static ComboBox PRIOR1,PRIOR2,PRIOR3,PRIOR4,PRIOR5,PRIOR6,PRIOR7,PRIOR8,PRIOR9,PRIOR10,PRIOR11,PRIOR12,PRIOR13,PRIOR14,PRIOR15,PRIOR16,PRIOR17,PRIOR18
+	@FXML private ComboBox PRIOR1,PRIOR2,PRIOR3,PRIOR4,PRIOR5,PRIOR6,PRIOR7,PRIOR8,PRIOR9,PRIOR10,PRIOR11,PRIOR12,PRIOR13,PRIOR14,PRIOR15,PRIOR16,PRIOR17,PRIOR18
 					,PRIOR19,PRIOR20,PRIOR21,PRIOR22,PRIOR23,PRIOR24,PRIOR25,PRIOR26,PRIOR27,PRIOR28,PRIOR29,PRIOR30,PRIOR31,PRIOR32,PRIOR33,PRIOR34
 					,PRIOR35,PRIOR36,PRIOR37,PRIOR38,PRIOR39,PRIOR40,PRIOR41,PRIOR42,PRIOR43,PRIOR44,PRIOR45,PRIOR46,PRIOR47,PRIOR48,PRIOR49;
 	
@@ -66,13 +67,15 @@ public class MyController implements Initializable{
 		//place is what line it is located with respect to corresponding values
 		while (place < checkboxes.length){
 			if( box == checkboxes[place][0]){
+				System.out.println("here");
 				j=0;
 				break;
-			} if( box == checkboxes[place][1]){
+			}else if( box == checkboxes[place][1]){
 				j=1;
 				break;
 			}
 			place++;
+			System.out.println(place);
 		}
 		
 		//set all fields to false, default colors appropriately, and set default values to 0.0*********************
@@ -104,11 +107,11 @@ public class MyController implements Initializable{
 		
 	}
 
-	public static void excel() throws IOException {
+	public void excel() throws IOException {
 		Row row;
 		Cell cell;
 		
-		File excel = new File("/home/oscar/Desktop/PMA.xls"); /**open file to read from master PMA **/
+		File excel = new File("PMA.xls"); /**open file to read from master PMA **/
 		FileInputStream fis = new FileInputStream(excel);	  /**create fileInputStream from master PMA file **/
 		
 		HSSFWorkbook wb = new HSSFWorkbook(fis); 			/**create new workbook based on master PMA **/	
@@ -123,6 +126,15 @@ public class MyController implements Initializable{
 		
 		cell = row.getCell(3);
 		cell.setCellValue(YEAR.getText());
+		
+		
+		fis.close();
+		
+		FileOutputStream fileOut = null;
+		fileOut = new FileOutputStream("/home/oscar/Desktop/workbook.xls");
+		wb.write(fileOut);
+		fileOut.close();
+		
 	}
 	
 	
