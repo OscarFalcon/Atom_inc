e1ef3f276c2c4579e961b1ac55fc6283c8d37494
john <john@john-PC>
2014-06-03 17:52:52 -0500

    submit

---
+++ src/PMAWizard/SelectCustomerController.java
@@ -0,0 +1,13 @@
+package PMAWizard;
+
+
+import javafx.event.ActionEvent;
+
+public class SelectCustomerController{
+	
+	public void createCustomer(ActionEvent event){
+		VistaNavigator.loadVista(VistaNavigator.CREATECUSTOMER);	
+		
+	}
+
+}
