9f831aad4440c0f54458e18b40993b16c744c325
oscarfalcon <oscar.falcon@live.com>
2014-06-04 00:09:11 -0500

    PMAWizard update: can now switch between screens.
    **Important **/
    We can now make nodes inside a main scene resize dynamically **/

--- src/PMAWizard/SelectCustomerController.java
+++ src/PMAWizard/SelectCustomerController.java
@@ -5,8 +5,10 @@ import javafx.event.ActionEvent;
 
 public class SelectCustomerController{
 	
+	
+	
+	
 	public void createCustomer(ActionEvent event){
-		VistaNavigator.loadVista(VistaNavigator.CREATECUSTOMER);	
 		
 	}
 
