9eec325cc957cc01094e132685ec2bdd3884e1d7
oscarfalcon <oscar.falcon@live.com>
2014-06-17 23:30:48 -0500

    oscar forgot to commit.

--- src/mycms/Type.java
+++ src/mycms/Type.java
@@ -1,43 +1,29 @@
 package mycms;
 
-import java.sql.Date;
 
-public class Type {
+public class Type<T> {
 
-	public static final int STRING = 0;
-	public static final int INTEGER = 1;
-	public static final int OBJECT = 2;
-	public static final int BYTE = 3;
-	public static final int PMA_OBJECT = 4;
-	public static final int DATE = 5;
 	
-	private Object value;
-	private int type;
-	
-	public Type(String S){
-		value = S;
-		type = STRING;
-	}
-	public Type(Integer I){
-		value = I;
-		type = INTEGER;
-	}
-	public Type(Object O){
-		value = O;
-		type = OBJECT;
-	}
-	public Type(Byte B){
-		value = B;
-		type = BYTE;
-	}
-	public Type(Date D){
-		value = D;
+	public enum Type_T{
+		BYTE,
+		DATE,
+		INTEGER,
+		STRING,
+		PMA_OBJECT,
+		OBJECT;
 	}
 	
-	public Object getValue(){
+	
+	
+	private T value;
+
+	public Type(T value){
+		this.value = value;
+	}
+	
+	public T getValue(){
 		return value;
 	}
-	public int getType(){
-		return type;
-	}
+	
+	
 }
--- src/pma/PMASettings.java
+++
@@ -1,5 +0,0 @@
-package pma;
-
-public class PMASettings {
-
-}
